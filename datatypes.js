5efbb08843acd6a8a45711ed9608aa9683ea6cc8
Dinesh Kumawat <114601860+Dineshk125@users.noreply.github.com>
2025-01-20 03:15:40 +0000

    datatypes ecma

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as new version
+
+// alert(5+5) // this is work on browser not in node 
+
+// eCMA Scripts Language
+
+// number = 1
+
+// BigInt
+
+// Boolean = true and false
+
+// string = ""
+
+// null = standalone CSSMathValue
+
+// undefined
+
+// Symbol = unique
+
+// Object
+
+console.table([typeof(""), typeof(unique), typeof(A), typeof(true), typeof(123), typeof(null), typeof(undefined)])
