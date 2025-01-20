40ae43807135cb1a2464989c117e11f04a592d35
Dinesh Kumawat <114601860+Dineshk125@users.noreply.github.com>
2025-01-20 03:28:59 +0000

    conversion

---
+++ 03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = 33
+
+console.log(typeof score)
+
+
+
+// "44"= 44
+// "44asd" NaN
+// true = 1; false = 0;
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let number = 33
+let stringNo = String(number)
+console.log(stringNo)
