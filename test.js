51f5ee09be80d7f821273496804905fea80174a4
Dinesh Kumawat <114601860+Dineshk125@users.noreply.github.com>
2025-01-20 02:57:47 +0000

    edit file

---
+++ test.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let costName = "Dinesh"
+var accountNo = 120987
+coustCity = "delhi"
+let state
+
+
+// accountId = 123456
+
+costName = "Kumawatji"
+accountNo = 1299e10
+coustCity = "jaipur"
+console.table([accountId, costName, accountNo, coustCity, state])
