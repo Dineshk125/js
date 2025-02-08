8d14e606dda5d757587a98b14de1e53db92ebb3f
Dineshk125 <dineshkumawatdtr123@gmail.com>
2025-02-08 13:51:31 +0530

    number and math

---
+++ 05_nums_and_maths.js
@@ -0,0 +1,54 @@
+const score = 400
+
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)    
+
+console.log(balance.toString().length)  
+
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.8989
+console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++= MATHS +++++++++++++++++++++++
+
+console.log(Math)
+
+console.log(Math.E)
+
+console.log(Math.PI)
+
+console.log(Math.SQRT2)
+
+console.log(Math.abs(-4))
+
+console.log(Math.round(4.3))
+
+console.log(Math.ceil(4.3))
+
+console.log(Math.floor(4.3))
+
+console.log(Math.min(3,4,5,6))
+
+console.log(Math.max(3,4,5,6))  
+
+console.log(Math.pow(2,3))
+
+console.log(Math.sqrt(25))
+
+console.log(Math.random())
+
+console.log(Math.floor(Math.random() * 100))
+
+console.log((Math.floor(Math.random() * 100) + 1))
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
+console.log(Math.floor(Math.random() * 10) + 1)
